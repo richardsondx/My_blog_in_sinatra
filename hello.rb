65a9595b8d96b49ad24f127a44c6399141b3e1de
evenix <richardsondx@gmail.com>
2012-08-06 21:46:34 -0400

    working

--- hello.rb
+++ hello.rb
@@ -1,7 +1,13 @@
+require 'rubygems'
 require 'sinatra'
 
-get '/' do
-  @posts = Post.ordered
-  @next_page = next_page
-  erb :index
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
 end
+
+require_relative 'config'
+require_relative 'routes'
